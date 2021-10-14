5b9959d8bc8d1e33c03d968dbc05bca5589c109d
Christoph Burgdorf <christoph.burgdorf@gmail.com>
2021-10-14 16:06:53 +0200

    Cleanup assertions

--- test/registry-tests.js
+++ test/registry-tests.js
@@ -32,13 +32,13 @@ describe("BountyRegistry", function () {
   it("Should initially be unlocked", async function () {
     [registry, eric, admin] = await deployRegistry()
 
-    expect(await registry.callStatic.is_locked()).to.equal(false);
+    expect(await registry.callStatic.is_locked()).to.be.false;
   });
 
   it("Should revert when trying to lock without minimum deposit", async function () {
     [registry, eric, admin] = await deployRegistry()
 
-    expect(await registry.callStatic.is_locked()).to.equal(false);
+    expect(await registry.callStatic.is_locked()).to.be.false;
 
     await expect(registry.lock()).to.be.reverted;
   });
@@ -46,11 +46,11 @@ describe("BountyRegistry", function () {
   it("Should lock", async function () {
     [registry, eric, admin] = await deployRegistry()
 
-    expect(await registry.callStatic.is_locked()).to.equal(false);
+    expect(await registry.callStatic.is_locked()).to.be.false;
 
     await registry.lock({value: ONE_ETH });
 
-    expect(await registry.callStatic.is_locked()).to.equal(true);
+    expect(await registry.callStatic.is_locked()).to.be.true;
   });
 
   it("Should release lock after timeout", async function () {
@@ -58,11 +58,11 @@ describe("BountyRegistry", function () {
 
     await registry.lock({value: ONE_ETH });
 
-    expect(await registry.callStatic.is_locked()).to.equal(true);
+    expect(await registry.callStatic.is_locked()).to.be.true;
 
     await mineBlocks(1001);
 
-    expect(await registry.callStatic.is_locked()).to.equal(false);
+    expect(await registry.callStatic.is_locked()).to.be.false;
   });
 
   it("Should revert if non-admin tries to register challenge", async function () {
@@ -70,7 +70,7 @@ describe("BountyRegistry", function () {
 
     let challenge = ethers.utils.getAddress("0x8ba1f109551bd432803012645ac136ddd64dba72")
 
-    expect(await registry.callStatic.is_open_challenge(challenge)).to.equal(false);
+    expect(await registry.callStatic.is_open_challenge(challenge)).to.be.false;
 
     await expect(registry.register_challenge(challenge)).to.be.reverted;
   });
@@ -82,7 +82,7 @@ describe("BountyRegistry", function () {
 
     await registry.connect(admin).register_challenge(challenge);
 
-    expect(await registry.callStatic.is_open_challenge(challenge)).to.equal(true);
+    expect(await registry.callStatic.is_open_challenge(challenge)).to.be.true;
 
     await expect(registry.remove_challenge(challenge)).to.be.reverted;
   });
@@ -92,11 +92,11 @@ describe("BountyRegistry", function () {
 
     let challenge = ethers.utils.getAddress("0x8ba1f109551bd432803012645ac136ddd64dba72")
 
-    expect(await registry.callStatic.is_open_challenge(challenge)).to.equal(false);
+    expect(await registry.callStatic.is_open_challenge(challenge)).to.be.false;
 
     await registry.connect(admin).register_challenge(challenge);
 
-    expect(await registry.callStatic.is_open_challenge(challenge)).to.equal(true);
+    expect(await registry.callStatic.is_open_challenge(challenge)).to.be.true;
   });
 
   it("Should revert if admin tries to remove challenge while it is locked", async function () {
@@ -106,7 +106,7 @@ describe("BountyRegistry", function () {
 
     await registry.connect(admin).register_challenge(challenge);
 
-    expect(await registry.callStatic.is_open_challenge(challenge)).to.equal(true);
+    expect(await registry.callStatic.is_open_challenge(challenge)).to.be.true;
 
     await registry.lock({value: ONE_ETH });
 
@@ -120,11 +120,11 @@ describe("BountyRegistry", function () {
 
     await registry.connect(admin).register_challenge(challenge);
 
-    expect(await registry.callStatic.is_open_challenge(challenge)).to.equal(true);
+    expect(await registry.callStatic.is_open_challenge(challenge)).to.be.true;
 
     await registry.connect(admin).remove_challenge(challenge);
 
-    expect(await registry.callStatic.is_open_challenge(challenge)).to.equal(false);
+    expect(await registry.callStatic.is_open_challenge(challenge)).to.be.false;
   });
 
   it("Should solve challenge and claim bounty", async function () {
@@ -142,7 +142,7 @@ describe("BountyRegistry", function () {
     })
 
     await registry.connect(admin).register_challenge(game.address);
-    expect(await registry.callStatic.is_open_challenge(game.address)).to.equal(true);
+    expect(await registry.callStatic.is_open_challenge(game.address)).to.be.true;
 
     let eric_initial_balance = await registry.provider.getBalance(eric.address);
     expect(await registry.provider.getBalance(registry.address)).to.equal(ethers.utils.parseEther("10"));
@@ -152,11 +152,11 @@ describe("BountyRegistry", function () {
     // It doesn't match exactly. Probably because of what is lost to tx fees
     expect(eric_new_balance.lte(eric_initial_balance.sub(ONE_ETH))).to.be.true;
     expect(await registry.provider.getBalance(registry.address)).to.equal(ethers.utils.parseEther("11"));
-    expect(await game.callStatic.is_solved()).to.equal(false);
+    expect(await game.callStatic.is_solved()).to.be.false;
 
     // Make winning move
     await game.move_field(15);
-    expect(await game.callStatic.is_solved()).to.equal(true);
+    expect(await game.callStatic.is_solved()).to.be.true;
     // Claim bounty
     await registry.claim(game.address);
 
@@ -176,13 +176,13 @@ describe("BountyRegistry", function () {
     [registry, eric, admin] = await deployRegistry()
 
     await registry.connect(admin).register_challenge(game.address);
-    expect(await registry.callStatic.is_open_challenge(game.address)).to.equal(true);
+    expect(await registry.callStatic.is_open_challenge(game.address)).to.be.true;
     await registry.lock({value: ONE_ETH });
-    expect(await game.callStatic.is_solved()).to.equal(false);
+    expect(await game.callStatic.is_solved()).to.be.false;
 
     // Make winning move
     await game.move_field(15);
-    expect(await game.callStatic.is_solved()).to.equal(true);
+    expect(await game.callStatic.is_solved()).to.be.true;
     // Admin can't claim as he doesn't have the lock
     await expect(registry.connect(admin).claim(game.address)).to.be.reverted;
   });
@@ -196,12 +196,12 @@ describe("BountyRegistry", function () {
     [registry, eric, admin] = await deployRegistry()
 
     await registry.connect(admin).register_challenge(game.address);
-    expect(await registry.callStatic.is_open_challenge(game.address)).to.equal(true);
-    expect(await game.callStatic.is_solved()).to.equal(false);
+    expect(await registry.callStatic.is_open_challenge(game.address)).to.be.true;
+    expect(await game.callStatic.is_solved()).to.be.false;
 
     // Make winning move
     await game.move_field(15);
-    expect(await game.callStatic.is_solved()).to.equal(true);
+    expect(await game.callStatic.is_solved()).to.be.true;
     // Claim bounty
     await expect(registry.claim(game.address)).to.be.reverted;
 
@@ -216,11 +216,11 @@ describe("BountyRegistry", function () {
     [registry, eric, admin] = await deployRegistry()
 
     await registry.lock({value: ONE_ETH });
-    expect(await game.callStatic.is_solved()).to.equal(false);
+    expect(await game.callStatic.is_solved()).to.be.false;
 
     // Make winning move
     await game.move_field(15);
-    expect(await game.callStatic.is_solved()).to.equal(true);
+    expect(await game.callStatic.is_solved()).to.be.true;
     // Claim bounty
     await expect(registry.claim(game.address)).to.be.reverted;
 
@@ -235,9 +235,9 @@ describe("BountyRegistry", function () {
     [registry, eric, admin] = await deployRegistry()
 
     await registry.connect(admin).register_challenge(game.address);
-    expect(await registry.callStatic.is_open_challenge(game.address)).to.equal(true);
+    expect(await registry.callStatic.is_open_challenge(game.address)).to.be.true;
     await registry.lock({value: ONE_ETH });
-    expect(await game.callStatic.is_solved()).to.equal(false);
+    expect(await game.callStatic.is_solved()).to.be.false;
     // Claim bounty
     await expect(registry.claim(game.address)).to.be.reverted;
 
@@ -270,7 +270,7 @@ describe("BountyRegistry", function () {
     const game = await deployGame([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 0, 15]);
 
     await registry.connect(admin).register_challenge(game.address);
-    expect(await registry.callStatic.is_open_challenge(game.address)).to.equal(true);
+    expect(await registry.callStatic.is_open_challenge(game.address)).to.be.true;
     await registry.lock({value: ONE_ETH });
 
     await expect(registry.connect(admin).withdraw()).to.be.reverted;
