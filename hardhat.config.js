86918f634ba8955dc693e1149c2784666692ad80
Christoph Burgdorf <christoph.burgdorf@gmail.com>
2022-05-17 11:50:05 +0200

    Port to ne hardhat-fe plugin

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,5 @@
 require("@nomiclabs/hardhat-waffle");
-require("@berlinvege/hardhat-fe");
+require("@developerdao/hardhat-fe");
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -19,4 +19,7 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: "0.8.4",
+  fe: {
+    compilerPath: "~/Documents/hacking/ef/fe/target/debug/fe"
+  }
 };
